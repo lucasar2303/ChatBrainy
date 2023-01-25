29be6b92179a0a5907fc474da43020447e348381
lucasar2303 <lucas-ar13@hotmail.com>
2023-01-25 18:43:28 -0300

    Login in app

--- app/src/main/java/com/example/chatbrainy/view/LoginActivity.kt
+++ app/src/main/java/com/example/chatbrainy/view/LoginActivity.kt
@@ -3,8 +3,13 @@ package com.example.chatbrainy.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.chatbrainy.R
 import com.example.chatbrainy.databinding.ActivityLoginBinding
 import com.example.chatbrainy.databinding.ActivitySplashBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
 
@@ -16,8 +21,54 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var messages : Array<String> = resources.getStringArray(R.array.messages)
+
         binding.btnSignup.setOnClickListener{
             startActivity(Intent(this, SignupActivity::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener{
+            val email: String = binding.etEmail.text.toString()
+            val password: String = binding.etPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                binding.tvError.setText(messages[0])
+            }else{
+                authUser()
+            }
+        }
+    }
+    fun authUser(){
+        val email: String = binding.etEmail.text.toString()
+        val password: String = binding.etPassword.text.toString()
+        var messages : Array<String> = resources.getStringArray(R.array.messages)
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener{task->
+            if (task.isSuccessful){
+                binding.progressBar.visibility = View.VISIBLE
+                binding.tvError.text = ""
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+
+            }else{
+                var error:String? = null
+                try {
+                    throw task.exception!!
+                }catch (e: java.lang.Exception){
+                    error = messages[8]
+                }
+                binding.tvError.text = error
+            }
+
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val activeUser : FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+        if (activeUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/example/chatbrainy/view/OnBoardingActivity.kt
+++ app/src/main/java/com/example/chatbrainy/view/OnBoardingActivity.kt
@@ -51,7 +51,7 @@ class OnBoardingActivity : AppCompatActivity() {
             }
             if (position == onBoardingData.size){
                 savePrefData()
-                startActivity(Intent(this, MainActivity::class.java))
+                startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
         }
